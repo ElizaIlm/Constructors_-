22427a6f97938341fc84837c1414b67e4b11cbc9
ilmasheva-eliza <ilmeliza36@gmai.com>
2025-09-25 19:26:45 +0500

    Разработанный класс Student.

---
+++ Constructors_ilmasheva/Classes/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_ilmasheva.Classes
+{
+    public class Student
+    {
+        public string Firstname = "";
+        public string Lastname = "";
+        public string Surname = "";
+        public bool Scholarship = false;
+        public int Course = 4;
+
+        public Student(string Firstname, string Lastname, string Surname)
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.Surname = Surname;
+        }
+
+
+        public Student(string Firstname, string Lastname, string Surname, bool Scholarship)
+        : this(Firstname, Lastname, Surname) // Вызов предыдущего конструктора
+        {
+            this.Scholarship = Scholarship;
+        }
+
+
+        public Student(string Firstname, string Lastname, string Surname, bool Scholarship, int Course)
+       : this(Firstname, Lastname, Surname, Scholarship) // Вызов предыдущего конструктора
+        {
+            this.Course = Course;
+        }
+
+
+        public string GetFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
